13383e4b2ba3020022b1ff649d367202307138b4
jhladkov <zhenua.gladkovv@gmail.com>
2023-07-10 17:10:36 +0300

    commit (video-19) i18next eslint

--- src/widgets/LangSwitcher/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames';
 import { useTranslation } from 'react-i18next';
 import React from 'react';
 import { Button, ThemeButton } from 'widgets/Button/Button';
-import styles from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
     className?: string
@@ -15,7 +14,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
     };
     return (
         <Button
-            className={classNames(styles.LangSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             theme={ThemeButton.CLEAR}
             onClick={toggle}
         >
